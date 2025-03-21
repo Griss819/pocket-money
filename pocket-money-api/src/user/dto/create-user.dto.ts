3fae1c8c7b7f43744c0dcb8c1288bb249fe6486c
Griss819 <rafaeliglesias819@gmail.com>
2025-03-21 16:06:53 -0400

    pocket-money-api: validating user inputs

--- pocket-money-api/src/user/dto/create-user.dto.ts
+++ pocket-money-api/src/user/dto/create-user.dto.ts
@@ -1,12 +1,35 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @Matches(/^[a-zA-Z0-9]+$/, {
+    message: 'El nombre de usuario solo puede contener letras y números.',
+  })
+  @MaxLength(24, { message: 'El nombre no debe exceder los 24 caracteres.' })
+  @Matches(/^\S*$/, {
+    message: 'El nombre de usuario no puede contener espacios en blanco.',
+  })
   name: string;
 
   @IsNotEmpty()
   email: string;
 
   @IsNotEmpty()
+  @Matches(/^[^'"<>]*$/, {
+    message: 'La contraseña no puede contener los caracteres \', ", < o >.',
+  })
+  @Matches(/^\S*$/, {
+    message: 'La contraseña no puede contener espacios en blanco.',
+  })
+  @Matches(/^(?=.*\d)(?=.*[A-Z])$/, {
+    message:
+      'La contraseña debe tener al menos un número, una letra mayúscula, un carácter no alfanumérico. ',
+  })
+  @MinLength(6, {
+    message: 'La contraseña debe contener entre 6 y 16 caracteres.',
+  })
+  @MaxLength(16, {
+    message: 'La contraseña debe contener entre 6 y 16 caracteres.',
+  })
   password: string;
 }
