bd9b6b05a8fb279bc3a48b7d95f40f7419c8d5cf
Griss819 <rafaeliglesias819@gmail.com>
2025-03-31 13:15:30 -0400

    pocket-money-client: internationalization

---
+++ pocket-money-client/src/app/[locale]/layout.tsx
@@ -0,0 +1,43 @@
+import { routing } from '@/i18n/routing';
+import { hasLocale, NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { getTranslations, setRequestLocale } from 'next-intl/server';
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({locale}));
+}
+
+export async function generateMetadata({params} : { params: { locale: string } }) {
+  const {locale} = await params;
+  const t = await getTranslations({locale, namespace: 'Metadata'});
+
+  return {
+    title: t('title'),
+    description: t('description'),
+  };
+}
+
+export default async function LocaleLayout({
+                                             children,
+                                             params
+                                           }: {
+  children: React.ReactNode;
+  params: Promise<{locale: string}>;
+}) {
+  // Ensure that the incoming `locale` is valid
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  // Enable static rendering
+  setRequestLocale(locale);
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
