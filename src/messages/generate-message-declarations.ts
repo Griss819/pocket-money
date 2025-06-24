b50fa0f507b09009893a9bacda244abe492207fd
Griss819 <rafaeliglesias819@gmail.com>
2025-06-24 13:18:01 -0400

    ...

--- src/messages/generate-message-declarations.ts
+++
@@ -1,6 +0,0 @@
-import createMessagesDeclaration from 'next-intl/dist/types/plugin/createMessagesDeclaration';
-
-createMessagesDeclaration([
-  './src/messages/es.json',
-  './src/messages/en.json'
-]);
